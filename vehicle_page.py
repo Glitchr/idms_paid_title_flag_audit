2b4dd12cb19ec5cc47201a27579c3e2be3006922
Manuel Maldonado <19945996+Glitchr@users.noreply.github.com>
2024-03-21 15:29:56 -0400

    Refactor notes loop and remove pagination
    -Expand the maximun amount of note rows that can be displayed instead of using pagination
    -Fix StaleReferences error that may appear in some of the loops

--- vehicle_page.py
+++ vehicle_page.py
@@ -16,18 +16,6 @@ class VehiclePage:
         """Select the back-page button to return to the inventory list"""
         return WebDriverWait(self.driver, 60).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "pageLevelNavigationBackButton")))
-    
-    @property
-    def get_vin(self):
-        """Get the vehicle's VIN from the details section"""
-        return WebDriverWait(self.driver, 60).until(
-            EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div[8]/div[1]/div/div[1]/div/div/div/div/div[1]/div[7]/div[1]/span[2]")))
-
-    @property
-    def get_stock_number(self):
-        """Get the vehicle's stock number from the details section"""
-        return WebDriverWait(self.driver, 60).until(
-            EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div[8]/div[1]/div/div[1]/div/div/div/div/div[1]/div[3]/span[2]")))
 
     @property
     def open_more_details(self):
@@ -57,14 +45,37 @@ class VehiclePage:
     def waitout_loading_screen(self):
         """Wait for the loading screen to disappear"""
         return WebDriverWait(self.driver, 10).until(
-            EC.invisibility_of_element_located((By.CLASS_NAME, "spinnerContent")))  
+            EC.invisibility_of_element_located((By.CLASS_NAME, "spinnerContent")))
+
+    @property
+    def records_per_page_dropdown(self):
+        """Select the records per page button in the notes tab"""
+        return WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#divNoteList > div.pagingWrapper > select")))
     
+    def get_vin(self):
+        """Get the vehicle's VIN from the details section"""
+        return WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div[8]/div[1]/div/div[1]/div/div/div/div/div[1]/div[7]/div[1]/span[2]/b")))
+
+    def get_stock_number(self):
+        """Get the vehicle's stock number from the details section"""
+        return WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div[8]/div[1]/div/div[1]/div/div/div/div/div[1]/div[3]/span[2]/b")))
+
+    def change_records_per_page(self):
+        """Change the value from 10 to 250 in the dropdown"""
+        self.records_per_page_dropdown.click()
+        value_250 = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#divNoteList > div.pagingWrapper > select > option:nth-child(7)")))
+        value_250.click()
+        
     def get_current_note_page(self):
         """Get the current notes page number"""
         page_number = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "curpagenumber")))
         
-        return int(page_number)
+        return int(page_number.text)
 
     def get_total_note_pages(self):
         """Get the total number of pages from the paging wrapper"""
@@ -82,32 +93,28 @@ class VehiclePage:
 
     def find_note_with_flag(self):
         """Find the note with the flag name in its description"""
-        self.waitout_loading_screen
+        self.open_more_details.click()
+        vin = self.get_vin().text
+        self.change_records_per_page()
         rows = self.notes_list
-
         current_row = 1
-        while current_row < len(rows):
-            if "PAID TITLE ON TRANSIT" in rows[current_row].find_element(By.CLASS_NAME, "descriptionText").text:
-                vin = self.get_vin
-                date = rows[current_row].find_element(By.CLASS_NAME, "noteDate")
-                date_text = date.text
-                extracted_date = datetime.strptime(date_text, "%Y-%m-%d")
+        audit = False
+
+        while current_row <= len(rows):
+            if "PAID" or "PAID TITLE ON TRANSIT" in rows[current_row].find_element(By.CLASS_NAME, "descriptionText").text:
+                date = rows[current_row].find_element(By.CLASS_NAME, "noteDate").text
+                extracted_date = datetime.strptime(date, "%m/%d/%Y%I:%M %p")
                 days_difference = (datetime.now() - extracted_date).days
 
                 if days_difference > 10:
-                    print(f"- {vin} / PAID flag was set more than 10 days ago.")
+                    print(f"\t{vin} / PAID flag was set more than 10 days ago.")
+                    audit = True
                 else:
-                    print(f"- {vin} / PAID flag set within 10 days.")
+                    print(f"\t{vin} / PAID flag set within 10 days.")
 
-            current_row += 1  # Increment the row count in every iteration
+                break
 
-            # Check if there's a next page
-            total_pages = self.get_total_note_pages()
-            current_page = self.get_current_note_page()
-            if current_page < total_pages and current_row == len(rows):
-                self.navigate_to_next_note_page()
-                rows = self.notes_list.find_elements(By.CLASS_NAME, "oneChild")  # Refresh rows
-                current_row = 1  # Reset the row count for the new page
+            current_row += 1 
 
-        return vin
+        return audit, vin
      
